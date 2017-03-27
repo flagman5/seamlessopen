fc8bd1aff3ef80fd470023231613c688afa3b615
flagman5 <flagman5@gmail.com>
2017-03-27 11:15:31 -0400

    Update crawl.php

--- backend/crawl.php
+++ backend/crawl.php
@@ -9,7 +9,27 @@ foreach($areas as $area) {
   $url = "https://www.zillow.com/".$area."/open-house/";
   
   $result = do_curl($url, "http://www.google.com");
+  crawl_result($result);
   
+  //now we do the paginate
+  $delim = '<li class="zsg-pagination-ellipsis"><span>...</span></li><li>';
+  $max_page_and_pages = explode($delim, $paginate_info);
+  
+  $max_page_info = $max_page_and_pages[1];
+  $max_paginate = preg_match("/\>(\d)\<\/a\>/", $max_page_info, $match);
+  $max = $match[0];
+  
+  for($i=2;$i<=$max;$i++) {
+    $url = "https://www.zillow.com/".$area."/open-house/".$i."_p/".;
+    $referrer = "https://www.zillow.com/".$area."/open-house/".$i-1."_p/".;
+    $result = do_curl($url,$referrer);
+    sleep(rand(3,15));
+    crawl_result($result);
+  }
+  
+}
+
+function crawl_result($result) {
   $delimiter = '<script type="application/ld+json">';
   $set = array_shift(explode($delimiter, $result));
   
@@ -29,7 +49,4 @@ foreach($areas as $area) {
     //['location']['address']['addressRegion']
     //['url']
   }
-  
-  //now we do the paginate
-  
-} 
+}
