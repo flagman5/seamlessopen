ad4fd47a383aa9d5f7bba6687eeabbde2f66b4b3
flagman5 <flagman5@gmail.com>
2017-04-24 20:24:51 -0400

    Update fb_find.php

--- backend/fb_find.php
+++ backend/fb_find.php
@@ -1,67 +1,69 @@
 <?php
 // Pass session data over.
-session_start();
-$realtor_search_array = array("realtor", "real estate agent", "real estate", "realty", "agent", "broker", "associate","owner", "team");
+function fb_find($agent_name) {
+  session_start();
+  $realtor_search_array = array("realtor", "real estate agent", "real estate", "realty", "agent", "broker", "associate","owner", "team");
 
-// Include the required dependencies.
-require_once( 'vendor/autoload.php' );
-include("db_config");
+  // Include the required dependencies.
+  require_once( 'vendor/autoload.php' );
+  include("db_config");
 
-// Initialize the Facebook PHP SDK v5.
-$fb = new Facebook\Facebook([
-  'app_id'                => '',
-  'app_secret'            => '',
-  'default_graph_version' => 'v2.8',
-]);
+  // Initialize the Facebook PHP SDK v5.
+  $fb = new Facebook\Facebook([
+    'app_id'                => '',
+    'app_secret'            => '',
+    'default_graph_version' => 'v2.8',
+  ]);
 
-$token = json_decode(file_get_contents("https://graph.facebook.com/oauth/access_token?client_id=&client_secret=&grant_type=client_credentials"));
-$fb->setDefaultAccessToken($token->access_token);
+  $token = json_decode(file_get_contents("https://graph.facebook.com/oauth/access_token?client_id=&client_secret=&grant_type=client_credentials"));
+  $fb->setDefaultAccessToken($token->access_token);
 
-try {
-  $response = $fb->get('/search?q=Maryalice+Ryan+real+estate&type=page');
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-  // When Graph returns an error
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-  // When validation fails or other local issues
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
-}
+  try {
+    $response = $fb->get('/search?q=Maryalice+Ryan+real+estate&type=page');
+  } catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+  } catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+  }
 
-//$output = $response->getGraphObject();
-$output = $response->getGraphEdge();
-$num_results = count($output);
-foreach($output as $graphNode) {
-        $page_id = $graphNode->getField('id');
-        $name = $graphNode->getField('name');
-        $company = array("Coldwell", "Banker", "Residential", "Brokerage", "Westfield", "East", "Office");
-        $confidence = 0;
-        if(contains($name, $company)) {
-                $confidence += 5;
-        }
+  //$output = $response->getGraphObject();
+  $output = $response->getGraphEdge();
+  $num_results = count($output);
+  foreach($output as $graphNode) {
+          $page_id = $graphNode->getField('id');
+          $name = $graphNode->getField('name');
+          $company = array("Coldwell", "Banker", "Residential", "Brokerage", "Westfield", "East", "Office");
+          $confidence = 0;
+          if(contains($name, $company)) {
+                  $confidence += 5;
+          }
 
-        if(contains($name, $realtor_search_array)) {
-                $confidence += 2;
-        }
+          if(contains($name, $realtor_search_array)) {
+                  $confidence += 2;
+          }
 
-        $page_details = $fb->get('/'.$id.'?fields=about,phone,emails');
-        $details = $page_details->getGraphObject();
-        $emails = $details->getField('emails');
-        $phone = $details->getField('phone');
+          $page_details = $fb->get('/'.$id.'?fields=about,phone,emails');
+          $details = $page_details->getGraphObject();
+          $emails = $details->getField('emails');
+          $phone = $details->getField('phone');
 
-        if($confidence >= 7 || $num_results == 1) {
-                 echo $emails[0];
-                 break;
-        }
-        else if($confidence == 2) {
-                preg_match("/\((\d\d\d)\)", $phone, $matches);
-                if(checkAreaCode($matches[1], $addressState)) {
-                        echo $emails[0];
-                        break;
-                }
+          if($confidence >= 7 || $num_results == 1) {
+                   return $emails[0];
+                   break;
+          }
+          else if($confidence == 2) {
+                  preg_match("/\((\d\d\d)\)", $phone, $matches);
+                  if(checkAreaCode($matches[1], $addressState)) {
+                          return $emails[0];
+                          break;
+                  }
 
-        }
+          }
+  }
 }
 
 //return
